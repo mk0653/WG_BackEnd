ebaa677ca44828ef2c261f9086d03214dc1a521b
金親 <rxc1123777@gmail.com>
2023-02-04 04:03:46 +0900

    lambd　デプロイ

---
+++ lambda/signup-link-lambda.js
@@ -0,0 +1,18 @@
+const dynamodb = require("aws-sdk/clients/dynamodb");
+const docClient = new dynamodb.DocumentClient();
+
+exports.signupLinkToDBHandler = async (event, context, callback) => {
+  var params = {
+    TableName: "User",
+    Item: {
+      id: event.userName,
+      email: event.request.userAttributes.email,
+    },
+  };
+  const result = await docClient.put(params).promise();
+  console.log(
+    `cognito user link to dynamodb id=${event.userName} email=${event.request.userAttributes.email} result=${result}`
+  );
+
+  callback(null, event);
+};
