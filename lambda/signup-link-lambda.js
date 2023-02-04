b726d06804d87fefe15a8c16c7bc60e28d7d648d
金親 <rxc1123777@gmail.com>
2023-02-04 23:24:28 +0900

    Update signup-link-lambda.js

--- lambda/signup-link-lambda.js
+++ lambda/signup-link-lambda.js
@@ -14,5 +14,4 @@ exports.signupLinkToDBHandler = async (event, context, callback) => {
     `cognito user link to dynamodb id=${event.userName} email=${event.request.userAttributes.email} result=${result}`
   );
   callback(null, event);
-  
 };
